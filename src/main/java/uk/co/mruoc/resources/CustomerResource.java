6c6654192d04c4d75c15fc1ee29d777e585e14f7
Michael Ruocco <michael.ruocco@hotmail.com>
2016-05-23 09:17:34 +0100

    fixes addition of swagger

--- src/main/java/uk/co/mruoc/resources/CustomerResource.java
+++ src/main/java/uk/co/mruoc/resources/CustomerResource.java
@@ -3,11 +3,16 @@ package uk.co.mruoc.resources;
 import com.codahale.metrics.annotation.Timed;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 import uk.co.mruoc.api.Customer;
 import uk.co.mruoc.jdbi.CustomerDao;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 @Path("ws/v1/customers")
 @Api(value = "ws/v1/customers", description = "Customer Maintenance")
@@ -23,18 +28,21 @@ public class CustomerResource {
 
     @GET
     @Path("/{accountNumber}")
-    @ApiOperation(value = "Get single customer", response = Customer.class)
+    @ApiOperation(value = "Get single customer")
     @Timed
-    public Customer getCustomer(@PathParam("accountNumber") String accountNumber) {
-        return customerDao.getCustomer(accountNumber);
+    public Response getCustomer(@PathParam("accountNumber") String accountNumber) {
+        Customer customer = customerDao.getCustomer(accountNumber);
+        return Response.ok().entity(customer).build();
     }
 
     @POST
-    @ApiOperation(value = "Post customer", response = Customer.class)
+    @ApiOperation(value = "Post single customer")
     @Timed
-    public Customer createCustomer(Customer customer) {
+    public Response createCustomer(@ApiParam Customer customer, @Context UriInfo info) {
         customerDao.insert(customer);
-        return customerDao.getCustomer(customer.getAccountNumber());
+        Customer newCustomer = customerDao.getCustomer(customer.getAccountNumber());
+        URI uri = info.getBaseUriBuilder().path("ws/v1/customers" + newCustomer.getAccountNumber()).build();
+        return Response.created(uri).entity(newCustomer).build();
     }
 
 }
